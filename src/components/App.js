7cba554e5760d7a43934088b1665d81318901471
evalynledezma <evalynledezmacode@gmail.com>
2020-10-19 15:52:48 -0600

    deep dive Props, State, and this

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import JournalList from "./journals/journal_list";
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React, Props, and State Deep Dive</h1>
+        <JournalList heading="List 1"/>
+
+      </div>
+    )
+  }
 }
-
-export default App;
